90314277904aa4ddc52b2fe225f45be37369434a
Knychtell Cortes <83411569+knychtell@users.noreply.github.com>
2023-02-27 03:25:39 -0500

    Feb 27,2023

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "me" Then
+            MsgBox("Hello")
+        Else
+
+            MsgBox("no")
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_MouseHover(sender As Object, e As EventArgs) Handles TextBox1.MouseHover
+        MsgBox("Yes")
+
+    End Sub
+End Class
